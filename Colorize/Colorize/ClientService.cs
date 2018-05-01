dfc43d0a32839407967e35be559c0d754325283d
sultankadyr <sultan.kadyrkesh@gmail.com>
2018-05-01 12:52:06 +0600

    3

---
+++ Colorize/Colorize/ClientService.cs
@@ -0,0 +1,17 @@
+﻿namespace Colorize
+{
+    public class ClientService
+    {
+        public Algorithmia.Client client;
+
+        public ClientService()
+        {
+            client = new Algorithmia.Client("sim+J7DtqBc23Imjz9t2k22tsJ21");
+        }
+
+        public Algorithmia.Client GetClient()
+        {
+            return client;
+        }
+    }
+}
--- Colorize/Colorize/Controllers/HomeController.cs
+++ Colorize/Colorize/Controllers/HomeController.cs
@@ -5,11 +5,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Colorize.Models;
+using Newtonsoft.Json.Linq;
 
 namespace Colorize.Controllers
 {
     public class HomeController : Controller
     {
+        public Algorithmia.Client client;
+        public HomeController(ClientService _client)
+        {
+            client = _client.GetClient();
+        }
         public IActionResult Index()
         {
             return View();
@@ -20,6 +26,24 @@ namespace Colorize.Controllers
             ViewData["Message"] = "Your application description page.";
 
             return View();
+        }
+
+        public IActionResult Color(string url)
+        {
+            try
+            {
+                var input = "{ " + "\"image\" :\"" + url + "\"}";
+                var algorithm = client.algo("deeplearning/ColorfulImageColorization/1.1.13");
+                var response = algorithm.pipeJson<object>(input);
+                JObject res = (JObject)response.result;
+                string output = (string)res["output"];
+                var file = client.file(output).getBytes();
+                return new OkObjectResult(file);
+            }
+            catch(Exception exp)
+            {
+                return new BadRequestObjectResult(exp.Message);
+            }
         }
 
         public IActionResult Contact()
